c808de27d1c8cb48d3ff77253cff5495b31174d0
DiegoTrapiello <diego_trapiello@hotmail.com>
2020-10-29 11:29:47 +0100

    Ejercicio 1

---
+++ Ejercicio01/Cabecera.js
@@ -0,0 +1,8 @@
+var asignatura = new Object();
+asignatura.nombre = "Software y estándares en la Web";
+asignatura.titulacion = "Grado en Ingeniería Informática del Software";
+asignatura.centro = "Escuela de Ingeniería Informática";
+asignatura.universidad = "Universidad de Oviedo";
+asignatura.curso = "2020-2021";
+asignatura.estudiante = "Diego Trapiello Mendoza";
+asignatura.email = "uo257915@uniovi.es";
