ce4b9797e3741b125ab2e3703efafabec0e1fde5
DiegoTrapiello <diego_trapiello@hotmail.com>
2020-11-07 12:15:15 +0100

    Validaciones Calculadora RPN

--- Ejercicio05/Calculadora.js
+++ Ejercicio05/Calculadora.js
@@ -46,11 +46,69 @@ class Calculadora
 
     enter(){
         this.valor = this.pantalla.value;
-        if (this.valor!=""){
+        if (this.valor!="" && this.memoria.count()<8){
             this.memoria.apilar(this.valor);
             this.pantalla.value = "";
         }
     }
+
+    sen(){
+        if(this.memoria.count()>0){
+            this.primero = this.memoria.desapilar();
+            this.resultado = Math.sin(this.primero);
+            this.memoria.apilar(this.resultado);    }
+      }
+  
+      cos(){
+        if(this.memoria.count()>0){
+            this.primero = this.memoria.desapilar();
+            this.resultado = Math.cos(this.primero);
+            this.memoria.apilar(this.resultado);    }
+      }
+  
+      tan(){
+        if(this.memoria.count()>0){
+            this.primero = this.memoria.desapilar();
+            this.resultado = Math.tan(this.primero);
+            this.memoria.apilar(this.resultado);    }
+      }
+  
+      arcsen(){
+        if(this.memoria.count()>0){
+            this.primero = this.memoria.desapilar();
+            this.resultado = Math.asin(this.primero);
+            this.memoria.apilar(this.resultado);    }
+      }
+  
+      arccos(){
+        if(this.memoria.count()>0){
+            this.primero = this.memoria.desapilar();
+            this.resultado = Math.acos(this.primero);
+            this.memoria.apilar(this.resultado);    }
+      }
+  
+      arctan(){
+        if(this.memoria.count()>0){
+            this.primero = this.memoria.desapilar();
+            this.resultado = Math.atan(this.primero);
+            this.memoria.apilar(this.resultado);    }
+      }
+      exponencial(){
+        if(this.memoria.count()>0){
+            this.primero = this.memoria.desapilar();
+            this.resultado = Math.exp(this.primero);
+            this.memoria.apilar(this.resultado);    }
+      }
+
+      limpiarUltimo(){
+            this.memoria.desapilar();
+            this.memoria.mostrarMemoria();
+      }
+
+      limpiarMemoria(){
+        this.memoria.limpiarTodo();
+        this.memoria.mostrarMemoria();
+      }
 }
 
 
@@ -65,6 +123,10 @@ class Pila{
 
     apilar(valor){
         this.pila.push(valor);
+        this.mostrarMemoria();
+    }
+
+    mostrarMemoria(){
         this.contenido.innerHTML="";
         var contador =0;
         for(var i in this.pila.reverse()){
@@ -78,15 +140,13 @@ class Pila{
         return (this.pila.pop());
     }
 
-    mostrar(){
-        var stringPila = "<h2>" +this.nombre +"</h2>" + "<ul>";
-        for (var i in this.pila) stringPila += "<li>Pila[" + i + "] = " + this.pila[i] + "</li>";
-        stringPila += "</ul>"
-        return stringPila;
-    }
     
     count(){
         return this.pila.length;
+    }
+
+    limpiarTodo(){
+        this.pila=new Array();
     }
 
 }
